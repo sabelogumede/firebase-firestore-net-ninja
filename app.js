9ce4caebf5456b1f3fbd3cf51f6d1d75f2a2a859
sabelogumede <iamsabelogumede@gmail.com>
2019-05-06 17:12:53 +0200

    added id delete cross event function

--- app.js
+++ app.js
@@ -6,15 +6,26 @@ function renderCafe(doc) {
 	let li = document.createElement('li');
 	let name = document.createElement('span');
 	let city = document.createElement('span');
+	let cross = document.createElement('div');
 	// setting data into our element/attribute
 	li.setAttribute('data-id', doc.id);
 	name.textContent = doc.data().name;
 	city.textContent = doc.data().city;
+	cross.textContent = 'x';
 	// append our elements
 	li.appendChild(name);
 	li.appendChild(city);
+	li.appendChild(cross);
 	//append to cafeList - document
 	cafeList.appendChild(li);
+
+	//delete data by Id from firestore db
+	cross.addEventListener('click', (e) => {
+		e.stopPropagation();
+		let id = e.target.parentElement.getAttribute('data-id');
+        //console.log(id);
+        db.collection('cafes').doc(id).delete();
+	})
 }
 
 
@@ -29,12 +40,12 @@ db.collection('cafes').get().then((snapshot) => {
 
 // saving data
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('cafes').add({
-        name: form.name.value,
-        city: form.city.value
-    });
-    // crear out input fields after adding them
-    form.name.value = '';
-    form.city.value = '';
+	e.preventDefault();
+	db.collection('cafes').add({
+		name: form.name.value,
+		city: form.city.value,
+	});
+	// crear out input fields after adding them
+	form.name.value = '';
+	form.city.value = '';
 })
