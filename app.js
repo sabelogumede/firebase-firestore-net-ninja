7f0a6f07e73f935a08da12ce0a77108817ef3748
sabelogumede <iamsabelogumede@gmail.com>
2019-05-06 16:57:11 +0200

    added a form input and adding data to firebase db script

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // create elements and render cafe
-function renderCafe(doc){
+function renderCafe(doc) {
 	let li = document.createElement('li');
 	let name = document.createElement('span');
 	let city = document.createElement('span');
 	// setting data into our element/attribute
 	li.setAttribute('data-id', doc.id);
 	name.textContent = doc.data().name;
-    city.textContent = doc.data().city;
-    // append our elements
-    li.appendChild(name);
-    li.appendChild(city);
-    //append to cafeList - document
-    cafeList.appendChild(li)
+	city.textContent = doc.data().city;
+	// append our elements
+	li.appendChild(name);
+	li.appendChild(city);
+	//append to cafeList - document
+	cafeList.appendChild(li);
 }
 
 
-// get data from the db
+// getting data from the db
 db.collection('cafes').get().then((snapshot) => {
 	// loop through each document
 	snapshot.docs.forEach(doc => {
 		//parse the data into our render function above
 		renderCafe(doc);
 	})
+})
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    // crear out input fields after adding them
+    form.name.value = '';
+    form.city.value = '';
 })
